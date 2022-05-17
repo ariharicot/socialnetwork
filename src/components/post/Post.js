b7c088bf895ede3a98ce147eabd6e64d2ca720f3
Bredon <arileo.pereira@gmail.com>
2022-05-17 11:42:37 +0200

    likebuttondynamise

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './post.css'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -6,7 +6,15 @@ import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import { Users } from "../../dummyData";
 
 function Post({post}) {
+    {/* encrease like button on click*/}
+    const [like, setLike ] = useState(post.like)
+    const [isLiked, setIsLiked ] = useState(false)
 
+        {/* decrease if it's clicked twice*/}
+    const likeHandler =()=>{
+        setLike(isLiked ? like-1 : like+1)
+        setIsLiked(!isLiked)
+    }
     return (
         <div className='post'>
             <div className='postWrapper'>
@@ -26,9 +34,9 @@ function Post({post}) {
                 </div>
                 <div className='postBottom'>
                     <div className='postBottomLeft'>
-                    <FavoriteIcon className="likeIcon" />
-                    <ThumbUpIcon className="likeIcon" />
-                    <span className='postLikeCounter'>{post.like}</span>
+                    <FavoriteIcon className="likeIcon" onClick={likeHandler} />
+                    <ThumbUpIcon className="likeIcon" onClick={likeHandler} />
+                    <span className='postLikeCounter'>{like}</span>
                     </div>
                     <div className='postBottomRight'>
                         <span className='postCommentLike'>{post.comment}</span>
