c6d24599ff151bbcb8e1f33606b9dea060689745
Bredon <arileo.pereira@gmail.com>
2022-05-17 10:19:32 +0200

    rigtbarstarted

--- src/components/rightbar/RightBar.js
+++ src/components/rightbar/RightBar.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './index.css'
+import CakeIcon from '@mui/icons-material/Cake';
 
 function RightBar() {
   return (
-    <div className='right-bar'>RightBar</div>
+    <div className='right-bar'>
+      <div className='rightBarWrapper'>
+        <div className='birthdayContainer'>
+          <CakeIcon className='birthdayImg' />
+          <span className='birthdayText'>C'est l'anniversaire d'<b className='birthdayFriend'>Andrea</b>.</span>
+        </div>
+        <h4 className='rightBarTitle'>Amis en ligne</h4>
+        <ul className='rightBarFriendList'>
+          <li className='rightBarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img className='rightBarFriendProfileImg' src="./assets/users/pexels-allan-mas-5368986.jpg" alt=""></img>
+              <span className='rightBarOnline'></span>
+            </div>
+            <span className='rightBarUserName'>Jean93</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
