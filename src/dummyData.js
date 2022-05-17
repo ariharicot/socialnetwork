e23f0325520487c65097e2c9850e1a1eccec4885
Bredon <arileo.pereira@gmail.com>
2022-05-17 10:57:34 +0200

    dummydatainplace

---
+++ src/dummyData.js
@@ -0,0 +1,123 @@
+
+export const Users = [
+    {
+        id:1,
+        profilePicture: './assets/users/pexels-mayim-luna-10883680.jpg',
+        username:"Emily Casa",
+    },
+    {
+        id: 2,
+        profilePicture: './assets/users/pexels-allan-mas-5368986.jpg',
+        username: "Jean Dupont",
+    },
+    {
+        id: 3,
+        profilePicture:'./assets/users/pexels-andrea-piacquadio-774909.jpg',
+        username:"Andrea Pesca",
+    },
+    {
+        id: 4,
+        profilePicture:'./assets/users/pexels-andrea-piacquadio-3769021.jpg',
+        username: "Marie Piacquadio",
+    },
+    {
+        id: 5,
+        profilePicture: './assets/users/pexels-dillon-kydd-5794559.jpg',
+        username:"Margot Martinez",
+    },
+    {
+        id: 6,
+        profilePicture: './assets/users/pexels-ekaterina-belinskaya-4923104.jpg',
+        username: "Jack Durand",
+    },
+    {
+        id: 7,
+        profilePicture: './assets/users/pexels-kampus-production-6670986.jpg',
+        username:"Nicolas Sanchez",
+    },
+    {
+        id: 8,
+        profilePicture: './assets/users/pexels-linkedin-sales-navigator-2182970.jpg',
+        username:"Pascal Brodie"
+    }
+    
+
+];
+
+export const Post = [
+    {
+        id: 1,
+        desc: "Retour au bureau",
+        photo: "./assets/posts/emily-2.jpg",
+        date: "Il y a deux semaines",
+        userId: 1,
+        like: 89,
+        comment: 60,
+    },
+
+    {
+        id: 2,
+        desc: "I'll be back",
+        photo:"./assets/posts/jean-post2.jpg",
+        date: "Il y a 15 minutes",
+        userId: 2,
+        like: 350,
+        comment: 280,
+    },
+
+    {
+        id: 3,
+        desc: "Merci les amis",
+        photo:"./assets/posts/andrea-post.jpg",
+        date:"Il y a 2 jours",
+        userId: 3,
+        like: 1800,
+        comment: 300,
+    },
+    {
+        id: 4,
+        desc: "Nouvelle Formation",
+        photo: "./assets/posts/marie-post.jpg",
+        date: "Il y a 30 minutes",
+        userId: 4,
+        like: 560,
+        comment: 25,
+    },
+    { 
+        id: 5,
+        desc: "Let's get back to business!",
+        photo: "./assets/posts/margot-post.jpg",
+        date: "Il y a 5 heures",
+        userId: 5,
+        like: 3001,
+        comment: 987,
+    },
+    {
+        id: 6,
+        desc: "React ou VueJS?",
+        photo: "./assets/posts/jack-post.jpg",
+        date: "Il y a trois jours",
+        userId: 6,
+        like: 950,
+        comment: 1300,
+    },
+    {
+        id: 7,
+        desc: "Projet sur Bordeaux",
+        photo: "./assets/posts/nicolas-post.jpg",
+        date: "Il y un mois",
+        userId: 7,
+        like: 1230,
+        comment: 200,
+    },
+    {
+        id: 8,
+        desc:"Nouvelle passion.",
+        photo: "./assets/posts/pascal-post.jpg",
+        date: "Il y a une semaine",
+        userId: 8,
+        like: 60,
+        comment: 21,
+    },
+
+]
